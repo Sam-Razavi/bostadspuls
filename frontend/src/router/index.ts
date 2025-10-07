9df830dbb0e861401bc673de7683d1cca707c4a3
Sam Sahbaei Razavi <h24sasah@du.se>
2025-10-07 14:00:00 +0000

    feat: add vue router and base layout

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
+      component: () => import("../views/DashboardView.vue"),
+    },
+    {
+      path: "/trends",
+      name: "trends",
+      component: () => import("../views/TrendsView.vue"),
+    },
+    {
+      path: "/regions",
+      name: "regions",
+      component: () => import("../views/RegionsView.vue"),
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: () => import("../views/MapView.vue"),
+    },
+  ],
+});
+
+export default router;
